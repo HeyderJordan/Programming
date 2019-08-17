d8663adfb8a50d5fb8a802c8424b343621a9bcd1
Jordan <heyderjordanw@gmail.com>
2019-08-17 13:00:31 -0500

    Day 7

---
+++ IntroToJavascript/learn-to-code.js
@@ -0,0 +1,62 @@
+//var name = "Jack";    
+//var age = 21;
+//var shootingScore= 45.6;
+//
+//var message = "Hi, my name is " + name + " and i am " + age + " years old!";
+
+//var firstName = "John";
+//var lastName = "Jacobjingle";
+//var dateOfBirth = "12-14-99";
+//var age = 19;
+//var profileImgUrl = "http://coolpicks.com/johnjacob.jpg:";
+//
+//var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age +"th birthday.";
+//
+//console.log(loginWelcomeMessage); 
+
+//var sum = 10 + 15;
+//var sub = 15-10;
+//var mul = 10 * 3;
+//var div = 10 / 3;
+//var mod = 10 % 3;
+//
+//var msg= "10 / 3 = 3 with a remainder of " + mod;
+//
+//
+//var result = 10 * ((5 + 3) - 4);
+//console.log(result);
+
+var myAccountBalance = 300;
+var nikeSBShoes = 779.23;
+var coupon = 500
+//== is equal to
+//<= less than or equal to
+//>=greater than or equal to
+//=== is equal to and check for same type
+
+var val1 = 23;
+var val2 = "23";
+
+if (val1 === val2){
+    console.log("These are the same.");
+    
+} else {
+    console.log("These are not the same.")
+}
+
+
+
+//if (nikeSBShoes <= myAccountBalance) {
+//    myAccountBalance -= nikeSBShoes;
+//    console.log("We just bought some dope shoes!");
+//    console.log("Account Balance: " + myAccountBalance);
+//} else if (nikeSBShoes - coupon <= myAccountBalance){
+//    myAccountBalance -= nikeSBShoes - coupon;
+//    console.log("We just bought some dope shoes with a coupon!");
+// console.log("Account Balance: " + myAccountBalance);
+//
+//}else {
+//    console.log("Transaction Declined");
+//}
+
+
